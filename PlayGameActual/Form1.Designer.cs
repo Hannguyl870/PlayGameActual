b3ea1b77ce0758becef392d146b617951b44ba94
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-09-22 11:20:18 -0400

    Add project files.

---
+++ PlayGameActual/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PlayGameActual
+{
+    partial class ground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Lime;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(287, 147);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(226, 163);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.outputLabel.Location = new System.Drawing.Point(333, 354);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(123, 16);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Press button to start";
+            // 
+            // ground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "ground";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ PlayGameActual/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGameActual
+{
+    public partial class ground : Form
+    {
+        public ground()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = true;
+            startButton.BackColor = Color.White;
+
+            outputLabel.Text = "3";
+            outputLabel.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(500);
+
+           
+            outputLabel.Text = "2";
+            outputLabel.BackColor = Color.Red;
+            Refresh();
+            Thread.Sleep(500);
+
+            outputLabel.Text = "1";
+            Refresh();
+            Thread.Sleep(500);
+
+         outputLabel.Text = "GO";
+
+        }
+    }
+}
